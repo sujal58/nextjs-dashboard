f1d802e7808511e203bb5a40b3b5a1a1b0399eac
sujal <pandeysujal258@gmail.com>
2025-04-09 09:11:10 +0545

    pagination and filtering added

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,22 +2,23 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(text: string) {
+  const handleSearch = useDebouncedCallback((text: string) => {
     const params = new URLSearchParams(searchParams);
-
+    params.set("page", "1");
     if (text) {
       params.set("query", text);
     } else {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
